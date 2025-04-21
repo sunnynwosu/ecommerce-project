e6f88ffabceba68a2dda817a76a97a75e82aa622
sunnynwosu <sunnynwosu@yahoo.com>
2025-04-21 08:11:38 +0100

    Update: terafform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,3 +16,19 @@ variable "aks_cluster_name" {
 variable "acr_name" {
     default = "ecommerceacr190425"
 }
+
+variable "client_id" {
+    default = "e6c46f87-9669-4388-9fa8-3d9b03e4d6ed"
+}
+
+variable "client_secret" {
+    default = "UKg8Q~zxExa4wwqkOPUjzaIyfbcQwFA._uuticN~"
+}
+
+variable "subscription_id" {
+    default = "391d643b-d63a-4d14-a4a2-004cf58a7a95"
+}
+
+variable "tenant_id" {
+    default = "ab6b7f31-434e-4c52-8fbc-4539a4673a9f" 
+}
