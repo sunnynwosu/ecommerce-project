8ae9536c7127beeb396954f1f721ac2828a0fefa
sunnynwosu <sunnynwosu@yahoo.com>
2025-04-26 18:09:23 +0100

    modification: actions mod

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,4 @@
 provider "azurerm" {
-    features {}
-
-  client_id = var.client_id
-  client_secret = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id = var.tenant_id   
+    features {} 
 }
   
